d5a9a4fb381388c3b5a3a4fff719a0d237553306
pritamkamble <7276279026.pk@gmail.com>
2021-03-04 21:29:28 +0530

    get farmer by id fixed

--- api/farmer/controller/farmer.js
+++ api/farmer/controller/farmer.js
@@ -46,7 +46,7 @@ exports.getAllFarmers = asyncMiddleware(async (req, res) => {
     return res.status(200).json(allFarmers);
 });
 exports.getFarmer = asyncMiddleware(async (req, res) => {
-    let farmerRecord = await farmer.findOne({ user: { _id: req.params.id } }).populate('user').populate('material');
+    let farmerRecord = await farmer.findOne({ _id: req.params.id }).populate('user').populate('material');
     if (!farmerRecord) return res.status(200).json({ message: 'No farmer record found!' });
     return res.status(200).json(farmerRecord);
 });
